ad3ce9fbc32dc6c07b10535c24a0a046c457db87
Nazar Zhuvaka <nazar.zhuvaka.mcboss@gmail.com>
2024-07-26 16:49:37 +0300

    cart

--- constants/catalog.js
+++ constants/catalog.js
@@ -1,50 +1,57 @@
 const CATALOG = [
-    {
-        id: 'el1',
-        name: 'Mérida Pallas',
-        imgURl: 'https://www.guitarcentre.cz/ew/ew_images/image?EwImage=984bfb7c-e6b1-43a9-b234-243fc6d80705&Filter=b6362aea-d318-435f-9a1a-25bbb19769df',
-        price: '16 890'
-    },
-    {
-        id: 'el2',
-        name: 'Pablo Vitaso VCG-16 Junior lesk',
-        imgURl: 'https://www.guitarcentre.cz/ew/ew_images/image?EwImage=19747f83-fcef-4684-9556-a8bc48ced6dc&Filter=b6362aea-d318-435f-9a1a-25bbb19769df',
-        price: '20 568'
-    },
-    {
-        id: 'el3',
-        name: 'LA Electric Guitar by Gear4music',
-        imgURl: 'https://r2.gear4music.com/media/42/429541/600/preview_2.jpg',
-        price: '2 726'
-    },
-    {
-        id: 'el4',
-        name: 'Eko guitars NXT A100ce Natural Elektroakustická kytara Jumbo',
-        imgURl: 'https://media.cdn.kaufland.de/product-images/1024x1024/d8276708494c7bf4916c7dfa3412060c.webp',
-        price: '4 728'
-    },
-    {
-        id: 'el5',
-        name: 'LAG Tramontane 98 T98A Natural Akustická kytara Jumbo',
-        imgURl: 'https://media.cdn.kaufland.de/product-images/1024x1024/00b9ac41407ad1d2e7fac2316957d55c.webp',
-        price: '8 388'
-    },
-    {
-        id: 'el6',
-        name: 'Ibanez FRH10N-IBF',
-        imgURl: 'https://media.cdn.kaufland.de/product-images/1024x1024/f7db0066de7973d3e036b736bd60d0a0.webp',
-        price: '5 678'
-    },
-    {
-        id: 'el7',
-        name: 'LAG Tramontane 118 T118D Black Akustická kytara',
-        imgURl: 'https://media.cdn.kaufland.de/product-images/1024x1024/daf5a65b5d57cdf2d65d4d91809e01a0.webp',
-        price: '10 689'
-    },
-    {
-        id: 'el8',
-        name: 'Stagg Scl60 Bk - Klasická Kytara',
-        imgURl: 'https://media.cdn.kaufland.de/product-images/1024x1024/b693ccef4e4b8fe65cb604bc62c62870.webp',
-        price: '2 990'
-    },
-]
+  {
+    id: "el1",
+    name: "Mérida Pallas",
+    imgURl:
+      "https://www.guitarcentre.cz/ew/ew_images/image?EwImage=984bfb7c-e6b1-43a9-b234-243fc6d80705&Filter=b6362aea-d318-435f-9a1a-25bbb19769df",
+    price: 16890,
+  },
+  {
+    id: "el2",
+    name: "Pablo Vitaso VCG-16 Junior lesk",
+    imgURl:
+      "https://www.guitarcentre.cz/ew/ew_images/image?EwImage=19747f83-fcef-4684-9556-a8bc48ced6dc&Filter=b6362aea-d318-435f-9a1a-25bbb19769df",
+    price: 20568,
+  },
+  {
+    id: "el3",
+    name: "LA Electric Guitar by Gear4music",
+    imgURl: "https://r2.gear4music.com/media/42/429541/600/preview_2.jpg",
+    price: 2726,
+  },
+  {
+    id: "el4",
+    name: "Eko guitars NXT A100ce Natural Elektroakustická kytara Jumbo",
+    imgURl:
+      "https://media.cdn.kaufland.de/product-images/1024x1024/d8276708494c7bf4916c7dfa3412060c.webp",
+    price: 4728,
+  },
+  {
+    id: "el5",
+    name: "LAG Tramontane 98 T98A Natural Akustická kytara Jumbo",
+    imgURl:
+      "https://media.cdn.kaufland.de/product-images/1024x1024/00b9ac41407ad1d2e7fac2316957d55c.webp",
+    price: 8388,
+  },
+  {
+    id: "el6",
+    name: "Ibanez FRH10N-IBF",
+    imgURl:
+      "https://media.cdn.kaufland.de/product-images/1024x1024/f7db0066de7973d3e036b736bd60d0a0.webp",
+    price: 5678,
+  },
+  {
+    id: "el7",
+    name: "LAG Tramontane 118 T118D Black Akustická kytara",
+    imgURl:
+      "https://media.cdn.kaufland.de/product-images/1024x1024/daf5a65b5d57cdf2d65d4d91809e01a0.webp",
+    price: 10689,
+  },
+  {
+    id: "el8",
+    name: "Stagg Scl60 Bk - Klasická Kytara",
+    imgURl:
+      "https://media.cdn.kaufland.de/product-images/1024x1024/b693ccef4e4b8fe65cb604bc62c62870.webp",
+    price: 2990,
+  },
+];
